3d8848069780994a6867b63aa7f1c4e22ed36ce6
Maxximusan <dragonstonecave@gmail.com>
2022-10-31 16:15:48 +0200

    2й - перенес форму отдельно

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from 'react'
+import { Form } from 'components/Form/Form'
+
+export class App extends React.Component {
+
+state = {
+  contacts: [],
+
+}
+
+  dataFormSubmit = data => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <Form submit={this.dataFormSubmit}/>
+      </>
+      )
+  };
+  // return (
+  //   <div
+  //     style={{
+  //       height: '100vh',
+  //       display: 'flex',
+  //       justifyContent: 'center',
+  //       alignItems: 'center',
+  //       fontSize: 40,
+  //       color: '#010101'
+  //     }}
+  //   >
+  //     React homework template
+  //   </div>
+  // );
 };
